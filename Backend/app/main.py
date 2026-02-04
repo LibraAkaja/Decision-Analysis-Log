87515c6e171bc086b12fd966de17d7922d474530
LibraAkaja <adhikaribiraj908@gmail.com>
2026-02-05 00:17:29 +0545

    Add corsmiddleware

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,6 +3,19 @@ from app.routers import decisions, options
 
 app = FastAPI(title="Decision API")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(decisions.router)
 app.include_router(options.router)
 
